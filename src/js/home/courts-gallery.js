ed54d02c0129eb7973a833fc05755cb5d9fcf47b
Milos-GoodGuy <milos@goodguys.nu>
2024-09-23 13:02:22 +0200

    add new courts gallery

--- src/js/home/courts-gallery.js
+++ src/js/home/courts-gallery.js
@@ -1,7 +1,7 @@
 var webflow = webflow || [];
 Webflow.push(function() {
 
-  var galleryThumbs = new Swiper('.gallery-thumbs1', {
+  var galleryThumbs1 = new Swiper('.gallery-thumbs1', {
     spaceBetween: 20,
     slidesPerView: 2,
     freeMode: true,
@@ -14,14 +14,14 @@ Webflow.push(function() {
       },
     }
   });
-  var galleryTop = new Swiper('.gallery-top1', {
+  var galleryTop1 = new Swiper('.gallery-top1', {
     spaceBetween: 10,
     thumbs: {
       swiper: galleryThumbs
     }
   });
   
-  var galleryThumbs = new Swiper('.gallery-thumbs2', {
+  var galleryThumbs2 = new Swiper('.gallery-thumbs2', {
     spaceBetween: 20,
     slidesPerView: 2,
     freeMode: true,
@@ -34,11 +34,38 @@ Webflow.push(function() {
       },
     }
   });
-  var galleryTop = new Swiper('.gallery-top2', {
+  var galleryTop2 = new Swiper('.gallery-top2', {
     spaceBetween: 10,
     thumbs: {
       swiper: galleryThumbs
     }
   });
+
+  let gallery3 = document.querySelector('.gallery-thumbs3');
+  let galleryswiper3 = document.querySelector('.gallery-top3');
+
+  if(gallery3 && galleryswiper3){
+    var galleryThumbs3 = new Swiper('.gallery-thumbs3', {
+      spaceBetween: 20,
+      slidesPerView: 2,
+      freeMode: true,
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
+      breakpoints: {
+        767: {
+          slidesPerView: 4,
+          spaceBetween: 20
+        },
+      }
+    });
+    var galleryTop3 = new Swiper('.gallery-top3', {
+      spaceBetween: 10,
+      thumbs: {
+        swiper: galleryThumbs
+      }
+    });
+  }
+
+
     
 });
